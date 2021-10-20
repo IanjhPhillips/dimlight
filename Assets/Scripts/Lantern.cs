bd96554c0d93b3281595ded521442e142f483b8e
IanjhPhillips <ianjhphillips@gmail.com>
2021-10-20 17:48:17 -0400

    feat: #6 add lantern script and link with player script

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour
+{
+
+    private bool active;
+    public float maxFuel;
+    private float currentFuel;
+    public float fuelConsumptionRate;
+    private float radius;
+
+    private Vector3 localScaleOn, localScaleOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = 10f;
+        //save these as to avoid creating new vectors every frame
+        localScaleOn = new Vector3(radius, radius, 1f);
+        localScaleOff = new Vector3(0f, 0f, 0f);
+
+        //initialize state
+        active = false;
+        transform.localScale = localScaleOff;
+        currentFuel = maxFuel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+            currentFuel -= fuelConsumptionRate;
+    }
+
+    //Set Lantern active state and scale (future: animation states)
+    public void SetActive(bool input)
+    {
+        if (active != input)
+        {
+            print("setting lantern: " + active);
+            active = input;
+            transform.localScale = active ? localScaleOn : localScaleOff;
+        }
+    }
+
+    //Mutators on radius, currentFuel and maxFuel
+    public float getRadius()
+    {
+        return currentFuel;
+        localScaleOn = new Vector3(radius, radius, 1f);
+    }
+
+    public void setRadius(float _radius)
+    {
+        radius = _radius;
+    }
+    public float getCurrentFuel ()
+    {
+        return currentFuel;
+    }
+
+    public void setCurrentFuel (float _fuel)
+    {
+        currentFuel = _fuel;
+    }
+
+    public float getMaxFuel()
+    {
+        return maxFuel;
+    }
+
+    public void setMaxFuel(float _maxFuel)
+    {
+        maxFuel = _maxFuel;
+    }
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,20 +8,36 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rigidBody;
     public Animator animator;
     Vector2 movement;
-    public bool spacebarStatus;
+    private bool spacebarStatus;
+
+    public GameObject lanternObj;
+    private Lantern lantern;
+
+    private void Start()
+    {
+        if (lanternObj == null)
+            lanternObj = GameObject.Find("Lantern");
+        lantern = lanternObj.GetComponent<Lantern>();
+    }
 
     // Update is called once per frame
     // Update will handle data inputs
     void Update()
     {
+        //get movement input vector
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement = movement.normalized;
+
+        //handle lantern input
         spacebarStatus = Input.GetKey("space");
+        UpdateLantern();
+
+        //set animator params
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-        animator.SetBool("SpacebarStatus",spacebarStatus);
+        animator.SetFloat("Speed", movement.magnitude);
+        //animator.SetBool("SpacebarStatus",spacebarStatus); //parameter does not yet exist. commenting to supress warnings
     }
 
     //Fixed Update is called based on a fixed timer (50 times a second default)
@@ -30,4 +46,11 @@ public class PlayerMovement : MonoBehaviour
     {
         rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    void UpdateLantern ()
+    {
+        lantern.SetActive(spacebarStatus);
+    }
+
+
 }
