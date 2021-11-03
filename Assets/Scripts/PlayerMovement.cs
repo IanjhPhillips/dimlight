87f2367f37b1f09cbbf01e137ef0c2a7b4a97cab
IanjhPhillips <ianjhphillips@gmail.com>
2021-11-03 14:08:46 -0400

    feat: #14 begin work on prototype scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float maxHealth = 10f;
     private float currentHealth;
-    public List<string> keys = new List<string>();
+    public List<Key.KeyColor> keys = new List<Key.KeyColor>();
 
     private void Start()
     {
